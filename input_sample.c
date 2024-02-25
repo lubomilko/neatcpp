f80cd521febfbd83d17ca79745f3b8a66bc47aa4
Lubomir Milko <lmilko@danfoss.com>
2024-02-25 13:00:45 +0100

    Minor sample comment changes

--- input_sample.c
+++ input_sample.c
@@ -1,24 +1,32 @@
 
 #define bla 0 /* This is a comment:
-    
+    #define NONEXISTENT_MACRO_A 5
     everything should be ignored. */
 
 #if 0
 #define MACRO_A 5
 #endif
 
+#if 0
+#else 2
+#endif
+
+
 /* another comment
+#define NONEXISTENT_MACRO_B 10
 */
 #define MACRO_X     (a + b)
 
 #define	MACRO_Y		(c + \
-    d)
+    d)\
 
 #define MACRO_Z     \
 	/* some formula */\
     (e + f) * \
     (g + h)
 
+/* another comment
+#define NONEXISTENT_MACRO_C 15 */
 
 #if A == 1
     #define MACRO_A 1
