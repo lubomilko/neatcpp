ee484ef8cfc62dbcf26e861dddbfd68680b88ddd
Lubomir Milko <lmilko@danfoss.com>
2024-02-29 18:08:21 +0100

    Add complex test expanding function defined using macros

--- test.py
+++ test.py
@@ -7,6 +7,11 @@ def main() -> None:
     with open("src1_out.c", "w", encoding="utf-8") as file:
         file.write(cpp.out_code)
 
+    cpp = CPreprocessor()
+    cpp.process_file("src2.c")
+    with open("src2_out.c", "w", encoding="utf-8") as file:
+        file.write(cpp.out_code)
+
 
 if __name__ == "__main__":
     main()
