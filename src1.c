4b88c7b7816b1b37fee0e561dc81abc0f0db9974
Lubomir Milko <lmilko@danfoss.com>
2024-03-01 14:47:40 +0100

    Add multiline test macros

--- src1.c
+++ src1.c
@@ -1,3 +1,7 @@
+#define MULTILINE_SIMPLE_MACRO \
+     a = 5 + 4;\
+     b = 4 + 3;
+
 #define VAR_MACRO(A, B, ...) A + B + func(__VA_ARGS__)
 
 #define xstr(s) str(s)
@@ -19,3 +23,11 @@ XAFTERX(BUFSIZE)    /* should be: X_1024 */
 
 /* expected: 1 + 2 + func(3, 4, 5) */
 VAR_MACRO(1, 2, 3, foo, 5)
+
+
+#define MULTILINE_FUNC_MACRO(a,b) \
+     (a) = 5 + 4;\
+     (b) = 4 + 3;
+
+  MULTILINE_SIMPLE_MACRO
+MULTILINE_FUNC_MACRO(A, B)
