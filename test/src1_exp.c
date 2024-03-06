2623a2288e0f128272f8d7c583b29a12bece4645
Lubomir Milko <lmilko@danfoss.com>
2024-03-06 19:07:07 +0100

    Add files with expected generated test outputs

---
+++ test/src1_exp.c
@@ -0,0 +1,19 @@
+  a = 5 + 4;
+  b = 4 + 3;
+
+     /* should be: "foo" */
+     "foo"
+     /* should be: "4" */
+     "4"
+
+X_BUFSIZE     /* should be: X_BUFSIZE */
+X_1024    /* should be: X_1024 */
+
+/* expected: 1 + 2 + func(3, 4, 5) */
+1 + 2 + func(3, 4, 5)
+
+  a = 5 + 4;
+  b = 4 + 3;
+    (A) = 5 + 4;
+
+    (B) = 4 + 3;
