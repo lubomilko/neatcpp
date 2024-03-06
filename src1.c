b142c9ccfe90d9d51293e86aa247f95781763b74
Lubomir Milko <lmilko@danfoss.com>
2024-03-06 11:58:48 +0100

    Add some line continuations to the test input source file

--- src1.c
+++ src1.c
@@ -1,8 +1,10 @@
 #define MULTILINE_SIMPLE_MACRO \
      a = 5 + 4;\
      b = 4 + 3;
-
-#define VAR_MACRO(A, B, ...) A + B + func(__VA_ARGS__)
+  MULTILINE_SIMPLE_MACRO   
+  
+#define VAR_MACRO(A, \
+     B, ...) A + B + func(__VA_ARGS__)
 
 #define xstr(s) str(s)
      #define str(s) # s
@@ -18,14 +20,16 @@
 #define TABLESIZE 1024
 #define BUFSIZE TABLESIZE
 
-AFTERX(BUFSIZE)     /* should be: X_BUFSIZE */
+AFTERX(BUFSIZE)     /* should be: X_BUFSIZE */    
 XAFTERX(BUFSIZE)    /* should be: X_1024 */
 
 /* expected: 1 + 2 + func(3, 4, 5) */
-VAR_MACRO(1, 2, 3, foo, 5)
+VAR_MACRO(1, \
+     2, 3, foo, 5)    
 
 
-#define MULTILINE_FUNC_MACRO(a,b) \
+#define MULTILINE_FUNC_MACRO(\
+     a,b) \
      (a) = 5 + 4;\
      \
      (b) = 4 + 3;
