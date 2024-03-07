73ec2e7332f3f71c5da95b1abeb3c89c52080707
Lubomir Milko <lmilko@danfoss.com>
2024-03-07 11:36:11 +0100

    Simplify test code by using common function to remove, write and compare file

--- test/test.py
+++ test/test.py
@@ -9,48 +9,46 @@ sys.path.append(str(Path(CURR_DIR_PATH, "..").resolve()))
 from cpreproc import CPreprocessor
 
 
-def compare_output(generated_file: Path, expected_file: Path) -> None:
-    gen_str = ""
+def remove_write_compare_file(out_file: Path, text: str, comp_file: Path) -> None:
+    try:
+        Path(out_file).unlink()
+    except FileNotFoundError:
+        pass
+    with open(out_file, "w", encoding="utf-8") as file:
+        file.write(text)
+    out_str = ""
     exp_str = ""
-    with open(generated_file, "r", encoding="utf-8") as file:
-        gen_str = file.read()
-    with open(expected_file, "r", encoding="utf-8") as file:
+    with open(out_file, "r", encoding="utf-8") as file:
+        out_str = file.read()
+    with open(comp_file, "r", encoding="utf-8") as file:
         exp_str = file.read()
 
-    if gen_str == exp_str:
-        print(f"OK: Generated output file '{generated_file.name}' matches "
-              f"the expected output file '{expected_file.name}'.")
+    if out_str == exp_str:
+        print(f"OK: Generated output file '{out_file.name}' matches "
+              f"the expected output file '{comp_file.name}'.")
     else:
-        print(f"ERROR: Generated output file '{generated_file.name}' does not match the "
-              f"expected output file '{expected_file.name}'.")
+        print(f"ERROR: Generated output file '{out_file.name}' does not match the "
+              f"expected output file '{comp_file.name}'.")
 
 
 def main() -> None:
     cpp = CPreprocessor()
     cpp.process_file(f"{CURR_DIR_PATH}/src1.c")
-    with open(f"{CURR_DIR_PATH}/src1_out.c", "w", encoding="utf-8") as file:
-        file.write(cpp.output)
-    compare_output(Path(CURR_DIR_PATH, "src1_out.c"), Path(CURR_DIR_PATH, "src1_exp.c"))
+    remove_write_compare_file(Path(CURR_DIR_PATH, "src1_out.c"), cpp.output, Path(CURR_DIR_PATH, "src1_exp.c"))
 
     cpp = CPreprocessor()
     cpp.add_include_dirs(f"{CURR_DIR_PATH}/incl_common")
     cpp.process_file(f"{CURR_DIR_PATH}/src2.c")
-    with open(f"{CURR_DIR_PATH}/src2_out.c", "w", encoding="utf-8") as file:
-        file.write(cpp.output)
-    compare_output(Path(CURR_DIR_PATH, "src2_out.c"), Path(CURR_DIR_PATH, "src2_exp.c"))
+    remove_write_compare_file(Path(CURR_DIR_PATH, "src2_out.c"), cpp.output, Path(CURR_DIR_PATH, "src2_exp.c"))
 
     cpp = CPreprocessor()
     cpp.add_include_dirs(f"{CURR_DIR_PATH}/incl_hstgr", f"{CURR_DIR_PATH}/incl_common")
     cpp.process_file(f"{CURR_DIR_PATH}/src3.c")
-    with open(f"{CURR_DIR_PATH}/src3_out.c", "w", encoding="utf-8") as file:
-        file.write(cpp.output)
-    compare_output(Path(CURR_DIR_PATH, "src3_out.c"), Path(CURR_DIR_PATH, "src3_exp.c"))
+    remove_write_compare_file(Path(CURR_DIR_PATH, "src3_out.c"), cpp.output, Path(CURR_DIR_PATH, "src3_exp.c"))
 
     cpp = CPreprocessor()
     cpp.process_file(f"{CURR_DIR_PATH}/src4.c")
-    with open(f"{CURR_DIR_PATH}/src4_out.c", "w", encoding="utf-8") as file:
-        file.write(cpp.output)
-    compare_output(Path(CURR_DIR_PATH, "src4_out.c"), Path(CURR_DIR_PATH, "src4_exp.c"))
+    remove_write_compare_file(Path(CURR_DIR_PATH, "src4_out.c"), cpp.output, Path(CURR_DIR_PATH, "src4_exp.c"))
 
 
 if __name__ == "__main__":
