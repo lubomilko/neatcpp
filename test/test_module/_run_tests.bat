b005a038f48193d4ba0fef9f5298f77fadfbce91
Lubomir Milko <lmilko@danfoss.com>
2024-03-09 15:02:00 +0100

    Add pushd and popd commands to bat file running module tests

--- test/test_module/_run_tests.bat
+++ test/test_module/_run_tests.bat
@@ -1,1 +1,9 @@
-pytest
+:: Store the current working dir and switch to the directory where this bat file is located, because all paths below are relative to this dir.
+:: %~dp0 contains the dir path of this bat file.
+@pushd %~dp0
+
+:: Execute tests.
+pytest test_module.py -v
+
+:: Switch back to the original current working directory.
+@popd
